d71cc5c69fbecc519c539e651979525c94a17e4b
Lucas Dev React Native <lucasdevreactnative@Mac-Pro-de-Lucas.local>
2022-05-02 22:09:28 -0300

    adicionando acessibilidade

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,5 +1,6 @@
 import { ChatTeardropDots } from "phosphor-react";
 import { useState } from "react";
+import { Popover } from "@headlessui/react";
 
 export function Widget() {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false);
@@ -9,20 +10,17 @@ export function Widget() {
   }
 
   return (
-    <div className="absolute bottom-4 right-4">
-      {isWidgetOpen && <p>Hello World</p>}
+    <Popover className="absolute bottom-4 right-4">
+      <Popover.Panel>Hello World</Popover.Panel>
 
-      <button
-        className="bg-brand rounded-full px-3 h-12 text-white flex items-center group"
-        onClick={toggleWidgetVisibility}
-      >
+      <Popover.Button className="bg-brand rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2"></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
